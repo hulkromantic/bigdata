ba2b63e5e80e123c0157ec9e3c278f3cba227322
hulkromantic <1572629341@qq.com>
2021-05-09 23:44:54 +0800

    demo redis commit

--- redis/src/main/java/com/slash/redis/config/RedisConfig2Test.java
+++ redis/src/main/java/com/slash/redis/config/RedisConfig2Test.java
@@ -11,6 +11,7 @@ import org.junit.runner.RunWith;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.annotation.Resource;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -41,13 +42,61 @@ public class RedisConfig2Test {
     @Test
     public void testObj() {
         UserVo userVo = new UserVo();
-        userVo.setName("test");
+        userVo.setName("jack");
         userVo.setAddress("beijing");
-        userVo.setAge(18);
+        userVo.setAge(24);
         ValueOperations operations = redisTemplate.opsForValue();
         redisService.expireKey("name", 20, TimeUnit.SECONDS);
         String key = RedisUtil2.getKey(UserVo.Table, "name", userVo.getName());
         UserVo vo = (UserVo) operations.get(key);
         System.out.println(vo);
+    }
+
+    @Test
+    public void testValueOption() {
+        UserVo userVo = new UserVo();
+        userVo.setName("jack");
+        userVo.setName("shanghai");
+        userVo.setAge(20);
+        valueOperations.set("testValue", userVo);
+        System.out.println(valueOperations.get("testValue"));
+    }
+
+    @Test
+    public void testSetOperation() throws Exception {
+        UserVo userVo = new UserVo();
+        userVo.setAddress("guangzhou");
+        userVo.setName("jack");
+        userVo.setAge(23);
+        UserVo auserVo = new UserVo();
+        auserVo.setAddress("beijing");
+        auserVo.setName("slash");
+        auserVo.setAge(25);
+        setOperations.add("user:test", userVo, auserVo);
+        Set<Object> result = setOperations.members("user:test");
+        System.out.println(result);
+    }
+
+
+    @Test
+    public void testHashOperation() {
+        UserVo userVo = new UserVo();
+        userVo.setName("jack");
+        userVo.setName("shenzheng");
+        userVo.setAge(20);
+        hashOperations.put("hash:user", userVo.hashCode() + "", userVo);
+        System.out.println(hashOperations.get("hash:user", userVo.hashCode() + ""));
+    }
+
+    @Test
+    public void ListOperations() throws Exception {
+        UserVo userVo = new UserVo();
+        userVo.setAddress("chengdu");
+        userVo.setName("jack");
+        userVo.setAge(23);
+        listOperations.leftPush("list:user", userVo);
+        System.out.println(listOperations.leftPop("list:user"));
+        // pop之后 值会消失
+        System.out.println(listOperations.leftPop("list:user"));
     }
 }
